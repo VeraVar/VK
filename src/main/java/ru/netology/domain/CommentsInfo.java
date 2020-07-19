4d87ca0986098b6e303d9dcd89fe454a3dd36a2d
Vera Var <primavera1919@yandex.ru>
2020-07-19 18:11:04 +0300

    Classes added

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private boolean canComment;
+
+    // + getters/setters
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int ownerId;
+    private int date;
+    private String text;
+    private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+    private ViewsInfo viewsInfo;
+    private GeoInfo geoInfo;
+    private boolean isFavorite;
+
+    // + getters/setters
+}
